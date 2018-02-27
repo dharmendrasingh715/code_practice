98756d9abdd377b360e6eb2d88a5f8e82c81da72
Dharmendra Singh Sikarwar <dsikarwar@sapient.com>
2018-02-27 16:24:02 +0530

    added module dependency code

---
+++ js/dep_management_module.js
@@ -0,0 +1,54 @@
+//A little module depedency manager
+
+var myModules = (function Manager(){
+	var modules = {};
+
+	function define(name,deps,impl){
+    	for(var i = 0; i < deps.length;i++) {
+			deps[i] = modules[deps[i]];
+		}
+		modules[name] = impl.apply(impl, deps);
+	}
+ 
+    function get(name) {
+		return modules[name];
+    }
+	
+	return {
+		define: define,
+		get: get
+	}
+})();
+
+//Usage
+
+MyModules.define( "bar", [], function(){
+	function hello(who) {
+		return "Let me introduce: " + who;
+	}
+
+	return {
+		hello: hello
+	};
+} );
+
+MyModules.define( "foo", ["bar"], function(bar){
+	var hungry = "hippo";
+
+	function awesome() {
+		console.log( bar.hello( hungry ).toUpperCase() );
+	}
+
+	return {
+		awesome: awesome
+	};
+} );
+
+var bar = MyModules.get( "bar" );
+var foo = MyModules.get( "foo" );
+
+console.log(
+	bar.hello( "hippo" )
+); // Let me introduce: hippo
+
+foo.awesome(); // LET ME INTRODUCE: HIPPO
